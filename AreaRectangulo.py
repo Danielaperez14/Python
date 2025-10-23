bc0833f71fc11f1861d2ed20833731ed85460868
Daniela Perez <danielaperez.s1401@gmail.com>
2025-10-23 12:00:50 -0500

    Area de un rectangulo

---
+++ AreaRectangulo.py
@@ -0,0 +1,8 @@
+print ("Calcular el área de un rectangulo")
+
+A = float(input("Ingrese la altura: "))
+B = float(input("Ingrese la base: "))
+
+Area = B * A
+
+print("El área del rectángulo es:", Area)
